bbe01f7e71ffdad39272c6cbb119e4f7f6d75c72
tanvipatel-16 <tanvipatel09112007@gmail.com>
2026-05-06 23:26:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-```python
+
 from flask import Flask, render_template, request, send_file
 import os
 from gtts import gTTS
@@ -62,4 +62,3 @@ def generate():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-```
